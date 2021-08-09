a5cd14cee2e409b4fc652315b074db8761576391
benjaminbtrance <benjaminb.tran@gmail.com>
2021-08-09 14:40:30 -0500

    add index, Category, Product, ProductTag, Tag models

--- models/Product.js
+++ models/Product.js
@@ -8,16 +8,44 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		product_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.DECIMAL(10, 2),
+			allowNull: false,
+			isDecimal: true,
+		},
+		stock: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			isNumeric: true,
+			defaultValue: 10,
+		},
+		category_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'category',
+				key: 'id',
+				unique: false,
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'product',
+	}
 );
 
 module.exports = Product;
